4d8ac4432ca0eb1b5c1e0906cfe3118e83b2cac1
aayushvermaa <aayush.verma@opstree.com>
2025-09-13 17:04:41 +0000

    add post hook code

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM aayush808/base-image-all-package-ub:0.2
+
+RUN groupadd -g 65522 buildpiper && \
+    useradd -u 65522 -g buildpiper -d /home/buildpiper -m buildpiper && \
+    chown -R buildpiper:buildpiper /home/buildpiper
+
+RUN mkdir -p \
+        /src/reports \
+        /bp/data \
+        /bp/execution_dir \
+        /opt/buildpiper/shell-functions \
+        /opt/buildpiper/data \
+        /bp/workspace && \
+    chown -R buildpiper:buildpiper /src /bp /opt
+
+
+
+ENV SLEEP_DURATION=5s
+
+
+COPY --chown=buildpiper:buildpiper build.sh /home/buildpiper/build.sh
+COPY --chown=buildpiper:buildpiper BP-BASE-SHELL-STEPS /opt/buildpiper/shell-functions/
+
+RUN chmod +x /home/buildpiper/build.sh && \
+    chown -R buildpiper:buildpiper /bp/workspace && \
+    mkdir -p /home/buildpiper/reports && \
+    chown -R buildpiper:buildpiper /home/buildpiper
+
+
+USER buildpiper
+
+
+WORKDIR /home/buildpiper
+
+
+ENTRYPOINT ["./build.sh"]
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Source common functions
+source /opt/buildpiper/shell-functions/functions.sh
+source /opt/buildpiper/shell-functions/log-functions.sh
+source /opt/buildpiper/shell-functions/str-functions.sh
+source /opt/buildpiper/shell-functions/file-functions.sh
+source /opt/buildpiper/shell-functions/aws-functions.sh
+
+# Enable Debugging if required
+if [ "$DEBUG" = true ]; then
+  set -x
+fi
+
+export POST_HOOK_CMD=$(getPostHookCommand)
+
+
+CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
+logInfoMessage "I'll $INSTRUCTION_TYPE the code available at [$CODEBASE_LOCATION]"
+sleep  $SLEEP_DURATION
+
+cd "${CODEBASE_LOCATION}" || { logErrorMessage "Failed to change directory to $CODEBASE_LOCATION"; exit 1; }
+
+#######################################################
+
+echo "$POST_HOOK_CMD" | while IFS= read -r cmd; do
+  if [ -n "$cmd" ]; then
+    logInfoMessage "Running: $cmd"
+    eval "$cmd" || logErrorMessage " Command failed: $cmd (continuing...)"
+  fi
+done
+
+TASK_STATUS=$?
+saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
+
+
